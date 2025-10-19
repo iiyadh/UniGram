336a01fee79282eb16267ae9217fb6d946bda107
Tabai Iyadh <tabaiiyadh317@gmail.com>
2025-10-19 12:46:12 +0100

    some fixes

--- auth-service/routes/authRoute.js
+++ auth-service/routes/authRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { login , logout , refresh } = require('../controllers/authController');
-const { authenticateToken , validateUID } = require('../middlewares/auth');
+const { authenticateToken } = require('../middlewares/auth');
 
 
 router.post('/login',login);
